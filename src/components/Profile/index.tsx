a55925c4ba2a67d1df781158649a5d9e52ca74d6
Matheus Motta <matheusmottaq@gmail.com>
2020-12-27 14:20:51 -0300

    about section

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,17 +1,45 @@
 import React from 'react';
-
-import { Container, Main, Image, About } from './styles';
+import { MdUpdate } from 'react-icons/md';
+import { Container, Main, Image, Info, About } from './styles';
+import profileImg from '../../assets/profile_1.jpg';
 
 const Profile: React.FC = () => {
   return (
     <Container>
       <Main>
-        <Image />
-        <About>
+        <Image
+          style={{
+            backgroundImage: `url(${profileImg})`,
+          }}
+        />
+        <Info>
           <h1>Katiele Rezer Menger</h1>
           <span>Doutoranda e Mestre em Ciência Política</span>
-        </About>
+
+          <div>
+            <MdUpdate />
+            <p>Última atualização do currículo em 19/11/2020</p>
+          </div>
+        </Info>
       </Main>
+      <About>
+        <h2>Sobre</h2>
+        <p>
+          Doutoranda e mestra em Ciência Política pelo Instituto de Estudos
+          Sociais e Políticos (IESP / UERJ). Bacharela em Relações
+          Internacionais pela Universidade Federal do Rio Grande do Sul (UFRGS).
+          Pesquisadora do Observatório Político Sul-Americano (OPSA) e do Núcleo
+          de Estudos de Teoria Social e América Latina (NETSAL), no qual é
+          coordenadora adjunta e compõe o subgrupo de Movimentos Sociais. Foi
+          pesquisadora de iniciação científica entre 2012 e 2015. Realizou
+          intercâmbio acadêmico na Facultad de Ciencias Políticas y Sociales da
+          Universidad Nacional Autónoma de México (UNAM) no período de 2015/1,
+          no curso de Relaciones Internacionales. Suas principais áreas de
+          interesse são estudos latino-americanos e estudos africanos, tráfico
+          de drogas e violência. Contato: mariliacloss@gmail.com (Texto
+          informado pelo autor)
+        </p>
+      </About>
     </Container>
   );
 };
--- src/components/Profile/styles.ts
+++ src/components/Profile/styles.ts
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 export const Container = styled.div`
   width: 80vw;
   max-width: 120rem;
-  display: flex;
   margin: 0 auto;
   align-items: center;
   justify-content: center;
@@ -23,9 +22,13 @@ export const Image = styled.div`
   background-color: #fff;
   border-radius: 1.4rem;
   box-shadow: 1px 1px 1px 1px rgba(230, 230, 230, 0.7);
+  background-size: cover;
+  background-size: contain auto;
+  background-repeat: no-repeat;
+  background-position: center;
 `;
 
-export const About = styled.div`
+export const Info = styled.div`
   width: 75%;
   background-color: #fff;
   border-radius: 1.4rem;
@@ -35,10 +38,45 @@ export const About = styled.div`
   h1 {
     font-size: 3rem;
     color: var(--text-secondary);
+    margin-bottom: 0.5rem;
   }
 
   span {
+    font-size: 1.6rem;
+    font-weight: 500;
+  }
+
+  div {
+    display: flex;
+    align-items: center;
+    margin-top: 2rem;
+
+    svg {
+      color: var(--text-secondary);
+    }
+
+    p {
+      font-size: 1.2rem;
+      margin-left: 0.5rem;
+    }
+  }
+`;
+
+export const About = styled.div`
+  width: 100%;
+  justify-content: space-between;
+  background-color: #fff;
+  border-radius: 1.4rem;
+  box-shadow: 1px 1px 1px 1px rgba(230, 230, 230, 0.7);
+  padding: 2%;
+  margin-top: 4vh;
+
+  h2 {
+    margin-bottom: 1.5rem;
+  }
+
+  p {
     font-size: 1.4rem;
-    font-weight: 400;
+    text-align: justify;
   }
 `;
