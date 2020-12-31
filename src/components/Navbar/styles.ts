a4dd18783c4502e9d7572aa589929b803322f571
Matheus Motta <matheusmottaq@gmail.com>
2020-12-31 15:01:58 -0300

    scrollbar overflow problem fixed

--- src/components/Navbar/styles.ts
+++ src/components/Navbar/styles.ts
@@ -3,11 +3,13 @@ import styled from 'styled-components';
 export const Container = styled.div`
   height: 6vh;
   display: flex;
+  max-width: 120rem;
   justify-content: center;
   align-items: center;
   border-radius: 1.4rem;
   background-color: var(--bg);
   margin: 2rem auto;
+  box-shadow: 1px 1px 1px 1px rgba(230, 230, 230, 0.7);
 
   a {
     text-decoration: none;
